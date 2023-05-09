8d4ec4e5890f8a8c45f80bf16c1bbe8b4e22d589
Nicholas <nickndolo95@gmail.com>
2023-05-09 15:36:38 +0300

    created AdminDashboard component

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,16 +1,16 @@
-// import React from 'react'
-//  import Header from '../AdminHeader/Header';
-//  import ListingTable from '../ListingTable/ListingTable';
-//  import Search from '../Search/Search';
+import React from 'react'
+ import Header from '../AdminHeader/Header';
+ import ListingTable from '../ListingTable/ListingTable';
+ import Search from '../Search/Search';
 
-// const AdminDashboard = ({vehicleList,  setVehicleList}) => {
-//   return (
-//     <div className="test">
-//        <Header />
-//       <Search/> 
-//        <ListingTable vehicleList={vehicleList}  setVehicleList={setVehicleList}/>
-//     </div>
-//   )
-// }
+const AdminDashboard = ({vehicleList,  setVehicleList}) => {
+  return (
+    <div className="test">
+       <Header />
+      <Search/> 
+       <ListingTable vehicleList={vehicleList}  setVehicleList={setVehicleList}/>
+    </div>
+  )
+}
 
-// export default AdminDashboard;
+export default AdminDashboard;
